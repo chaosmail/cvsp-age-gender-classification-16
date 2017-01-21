c2cc0cb92bc2ddc07d34c55ab0cd9b7f182fa386
Patrick <patrick.wahrmann@gmail.com>
2017-01-21 17:01:46 +0100

    Load Quiver models correctly

--- cnn-keras-update/startQuiver.py
+++ cnn-keras-update/startQuiver.py
@@ -13,15 +13,26 @@ import utils
 test_batchsize = 25
 
 DATASET_DIR = '../data/imdb-wiki-tiny-dataset'
-MODEL_DIR = './results/2017-01-20_12:58:44'
+MODEL_DIR = './results/2017-01-21_13:17:10'
 
 # Load latest model
-latest_model = max( fs.find('*.h5', path=MODEL_DIR), key=fs.ctime)
+latest_model_path = max( fs.find('*.h5', path=MODEL_DIR), key=fs.ctime)
+latest_model = load_model(latest_model_path, custom_objects={'LRN':LRN})
+
+# Remove slashes from the layer names
+for layer in latest_model._collected_trainable_weights:
+  layer.name = layer.name.replace('/', '')
+for layer in latest_model.layers:
+  layer.name = layer.name.replace('/', '')
+# for layer in latest_model.layers_by_depth:
+#   layer[0].name = layer[0].name.replace('/', '')
+for layer in latest_model.weights:
+  layer.name = layer.name.replace('/', '')
 
 # Visualize model
 # utils.visualise_with_quiver(load_model(latest_model), '/home/patrick', class_type='gender')
 gender = False
 if gender:
-  utils.visualise_with_quiver(load_model(latest_model, custom_objects={'LRN':LRN}), class_type='gender')
+  utils.visualise_with_quiver(latest_model, class_type='gender')
 else:
-  utils.visualise_with_quiver(load_model(latest_model, custom_objects={'LRN':LRN}), class_type='age')
+  utils.visualise_with_quiver(latest_model, class_type='age')
